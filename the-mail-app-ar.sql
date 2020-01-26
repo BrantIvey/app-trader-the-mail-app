bd5779d6f7aed28decfc09a8746370adfd01f8dc
annrinTN <annrumsey@comcast.com>
2020-01-26 01:06:04 -0600

    prepare to pull master

--- the-mail-app-ar.sql
+++ the-mail-app-ar.sql
@@ -1,4 +1,5 @@
 -- Review table contents for column matches and type matches with OUTER JOIN
+--Line up matching fields to compare row contents side by side
 SELECT a.name AS a_name, p.name AS p_name, 
        a.price AS a_price, p.price AS p_price, 
        a.review_count AS a_review_count, p.review_count AS p_review_count, 
@@ -9,5 +10,20 @@ SELECT a.name AS a_name, p.name AS p_name,
 	   FROM app_store_apps AS a
 FULL OUTER JOIN play_store_apps AS p
 	   ON a.name = p.name;
+	   
+--INFORMATION app_store_apps
+SELECT table_name, column_name, column_default, data_type, ordinal_position
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'app_store_apps'
+ORDER BY ORDINAL_POSITION
 
+--INFORMATION play_store_apps
+SELECT table_name, column_name, column_default, data_type, ordinal_position
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'play_store_apps'
+ORDER BY ORDINAL_POSITION
+	  
+
+	  
+	  
 	  
