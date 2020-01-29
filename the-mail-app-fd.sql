3d173f0b642a8aa2e74f3f6ff61edafaf91f0c1d
helimili@gmail.com <helimili@gmail.com>
2020-01-28 18:21:20 -0600

    Fianal 1st project

--- the-mail-app-fd.sql
+++ the-mail-app-fd.sql
@@ -1,12 +1,12 @@
 with cte as (
 	select distinct name,
-	p.genres as play_store_genre,
-	a.primary_genre as app_store_genre,
+	p.genres as play_store_genre, a.primary_genre as app_store_genre,
 	round((p.rating + a.rating)/2,2) as avg_rating,
 	p.review_count as play_store_review_count,
 	install_count,
 	replace(p.price, '$','')::numeric as price,
 	cast(a.review_count as int) as app_store_review_count,
+			 
 	CASE WHEN p.rating <= .24 THEN '12'
 	WHEN p.rating >= .25 AND p.rating <=.74 THEN '24'
 	WHEN p.rating >= .75 AND p.rating <=1.24 THEN '36'
@@ -19,6 +19,7 @@ with cte as (
 	WHEN p.rating >= 4.25 AND p.rating <=4.74 THEN '120'
 	WHEN p.rating >= 4.75 THEN '132'
 	ELSE p.rating :: numeric END AS P_lifespan_months,
+	
 	CASE WHEN a.rating <= .24 THEN '12'
 	WHEN a.rating >= .25 AND a.rating <=.74 THEN '24'
 	WHEN a.rating >= .75 AND a.rating <=1.24 THEN '36'
@@ -31,21 +32,38 @@ with cte as (
 	WHEN a.rating >= 4.25 AND a.rating <=4.74 THEN '120'
 	WHEN a.rating >= 4.75 THEN '132'
 	ELSE p.rating :: numeric END AS A_lifespan_months,
+	
 	case when replace(p.price, '$','')::numeric <=1 then 10000
 	else replace(p.price, '$','')::numeric * 10000
 	END as purchase_price,
+	
 	'9000'::int as monthly_net_revenue
+	
 from play_store_apps as p
 inner join app_store_apps as a using (name)
 order by name, p.review_count desc)
+
 select
 	distinct name,
 	price,
 	avg_rating,
 	play_store_genre,
 	app_store_genre,
-	purchase_price,
-	monthly_net_revenue * round((A_lifespan_months+P_lifespan_months)/2,2) - purchase_price as net_revenue
-from cte
-order by net_revenue desc
-limit 10
+	purchase_price, 
+	A_lifespan_months,
+	P_lifespan_months,
+	monthly_net_revenue * round((A_lifespan_months + P_lifespan_months)/2,2) - purchase_price as net_revenue,
+-- Rank observations may be made with combinations of fields or solo fields, commenting out as reviewing
+-- Apps recommended for purchase based on net_revenue are 10+ years old with a mix of rating & review counts	
+	RANK() OVER(ORDER BY avg_rating DESC) AS rating_rank,
+	RANK() OVER(ORDER BY app_store_review_count DESC) AS a_review_count_rank
+-- Optional Rank fields	
+	-- RANK() OVER(ORDER BY play_store_review_count DESC) AS p_review_count_rank
+	-- RANK() OVER(ORDER BY A_lifespan_months DESC) AS A_lifespan_rank,
+	-- RANK() OVER(ORDER BY P_lifespan_months DESC) AS P_lifespan_rank,
+-- Running sum of the Purchase Price and Net Revenue
+-- SUM (purchase_price) over (order by purchase_price 
+							  -- rows unbounded preceding) as cumulative_purchase_price
+FROM cte
+order by net_revenue desc, rating_rank
+LIMIT 10;
